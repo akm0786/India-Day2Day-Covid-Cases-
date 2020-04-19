bb11c0c8aed014e790337652a7453bf65b418bb8
Abhishek mishra <44899517+akm0786@users.noreply.github.com>
2020-04-19 22:43:24 +0530

    other links added

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ background-image: linear-gradient(147deg, #FFE53B 0%, #FF2525 74%);
 
 		<h3 class="text-uppercase p-3 text-center d-flex justify-content-between " 
 
-"> <a href="#">world-wide cases</a><a href="#">india cases</a></h3>
+"> <a href="worldwidecovid19.herokuapp.com"target="_blank">world-wide cases</a><a href="indiacovid19pandemic.herokuapp.com"target="_blank">india cases</a></h3>
 
 	
 
